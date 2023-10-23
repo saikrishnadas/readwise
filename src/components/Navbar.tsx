b0ccd2a9e65124c47a3e589abdef6de2861bcbd4
Sai <sai@SAIs-MacBook-Pro.local>
2023-10-23 12:33:43 +0530

    adding up and down page navigation in pdf

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,48 +1,61 @@
 import Link from "next/link";
 import MaxWidthWrapper from "./MaxWidthWrapper";
-import { LoginLink, RegisterLink } from "@kinde-oss/kinde-auth-nextjs/server";
+import {
+	LoginLink,
+	RegisterLink,
+	getKindeServerSession,
+} from "@kinde-oss/kinde-auth-nextjs/server";
 import { ArrowRight } from "lucide-react";
 import { buttonVariants } from "@/components/ui/button";
 
 const Navbar = () => {
+	const { getUser } = getKindeServerSession();
+	const user = getUser();
 	return (
 		<nav className="sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
 			<MaxWidthWrapper>
-				<div className="flex h-14 items-center justify-between border-b border-zinc-200">
-					<Link href="/" className="flex z-40 font-semibold">
-						<span>Readwise.</span>
-					</Link>
-
-					<div className="hidden items-center space-x-4 sm:flex">
-						<>
-							<Link
-								href="/pricing"
-								className={buttonVariants({
-									variant: "outline",
-									size: "sm",
-								})}
-							>
-								Pricing
-							</Link>
-							<LoginLink
-								className={buttonVariants({
-									variant: "ghost",
-									size: "sm",
-									className: "bg-green-400 hover:bg-green-200",
-								})}
-							>
-								Sign in
-							</LoginLink>
-							<RegisterLink
-								className={buttonVariants({
-									size: "sm",
-								})}
-							>
-								Get started <ArrowRight className="ml-1.5 h-5 w-5" />
-							</RegisterLink>
-						</>
+				{!user ? (
+					<div className="flex h-14 items-center justify-between border-b border-zinc-200">
+						<Link href="/" className="flex z-40 font-semibold">
+							<span>Readwise.</span>
+						</Link>
+						<div className="hidden items-center space-x-4 sm:flex">
+							<>
+								<Link
+									href="/pricing"
+									className={buttonVariants({
+										variant: "outline",
+										size: "sm",
+									})}
+								>
+									Pricing
+								</Link>
+								<LoginLink
+									className={buttonVariants({
+										variant: "ghost",
+										size: "sm",
+										className: "bg-green-400 hover:bg-green-200",
+									})}
+								>
+									Sign in
+								</LoginLink>
+								<RegisterLink
+									className={buttonVariants({
+										size: "sm",
+									})}
+								>
+									Get started <ArrowRight className="ml-1.5 h-5 w-5" />
+								</RegisterLink>
+							</>
+						</div>
 					</div>
-				</div>
+				) : (
+					<div className="flex h-14 items-center justify-center border-b border-zinc-200">
+						<Link href="/" className="flex z-40 font-semibold">
+							<span>Readwise.</span>
+						</Link>
+					</div>
+				)}
 			</MaxWidthWrapper>
 		</nav>
 	);
--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -1,44 +1,135 @@
-'use client'
+"use client";
 
-import React from "react";
-import {Document,Page} from "react-pdf";
+import { useState } from "react";
+import { Document, Page, pdfjs } from "react-pdf";
+import { useResizeDetector } from "react-resize-detector";
 
-import {Loader2} from 'lucide-react'
-import { useToast } from './ui/use-toast'
+import { ChevronDown, ChevronUp, Loader2 } from "lucide-react";
+import { useToast } from "./ui/use-toast";
 
-import 'react-pdf/dist/Page/AnnotationLayer.css';
-import 'react-pdf/dist/Page/TextLayer.css';
+import "react-pdf/dist/Page/AnnotationLayer.css";
+import "react-pdf/dist/Page/TextLayer.css";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { cn } from "@/lib/utils";
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 interface PdfRendererProps {
-	url: string
-  }
-
+	url: string;
+}
 
 function PdfRenderer({ url }: PdfRendererProps) {
-	const { toast } = useToast()
-	return <div ref={ref}>
-            <Document
-              loading={
-                <div className='flex justify-center'>
-                  <Loader2 className='my-24 h-6 w-6 animate-spin' />
-                </div>
-              }
-              onLoadError={() => {
-                toast({
-                  title: 'Error loading PDF',
-                  description: 'Please try again later',
-                  variant: 'destructive',
-                })
-              }}
-              file={url}
-              className='max-h-full'>
+	const { toast } = useToast();
+	const { width, ref } = useResizeDetector();
 
-				<Page
-                  width={width ? width : 1}
-                  pageNumber={1}
-                />
-            </Document>
-			</div>;
+	const [numPages, setNumPages] = useState<number>();
+	const [currPage, setCurrPage] = useState<number>(1);
+
+	const customPageValidator = z.object({
+		page: z
+			.string()
+			.refine((num) => Number(num) > 0 && Number(num) <= numPages!),
+	});
+
+	type TCustomPageValidator = z.infer<typeof customPageValidator>;
+
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+		setValue,
+	} = useForm<TCustomPageValidator>({
+		defaultValues: {
+			page: "1",
+		},
+		resolver: zodResolver(customPageValidator),
+	});
+
+	const handlePageSubmit = ({ page }: TCustomPageValidator) => {
+		setCurrPage(Number(page));
+		setValue("page", String(page));
+	};
+
+	return (
+		<div className="w-full bg-white rounded-md shadow flex flex-col items-center">
+			<div className="h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2">
+				<div className="flex items-center gap-1.5">
+					<Button
+						disabled={currPage <= 1}
+						onClick={() => {
+							setCurrPage((prev) => (prev - 1 > 1 ? prev - 1 : 1));
+							setValue("page", String(currPage - 1));
+						}}
+						variant="ghost"
+						aria-label="previous page"
+					>
+						<ChevronDown className="h-4 w-4" />
+					</Button>
+
+					<div className="flex items-center gap-1.5">
+						<Input
+							{...register("page")}
+							className={cn(
+								"w-12 h-8",
+								errors.page && "focus-visible:ring-red-500"
+							)}
+							onKeyDown={(e) => {
+								if (e.key === "Enter") {
+									handleSubmit(handlePageSubmit)();
+								}
+							}}
+						/>
+						<p className="text-zinc-700 text-sm space-x-1">
+							<span>/</span>
+							<span>{numPages ?? "x"}</span>
+						</p>
+					</div>
+
+					<Button
+						disabled={numPages === undefined || currPage === numPages}
+						onClick={() => {
+							setCurrPage((prev) =>
+								prev + 1 > numPages! ? numPages! : prev + 1
+							);
+							setValue("page", String(currPage + 1));
+						}}
+						variant="ghost"
+						aria-label="next page"
+					>
+						<ChevronUp className="h-4 w-4" />
+					</Button>
+				</div>
+			</div>
+
+			<div className="flex-1 w-full max-h-screen">
+				<div ref={ref}>
+					<Document
+						loading={
+							<div className="flex justify-center">
+								<Loader2 className="my-24 h-6 w-6 animate-spin" />
+							</div>
+						}
+						onLoadSuccess={({ numPages }) => setNumPages(numPages)}
+						onLoadError={() => {
+							toast({
+								title: "Error loading PDF",
+								description: "Please try again later",
+								variant: "destructive",
+							});
+						}}
+						file={url}
+						className="max-h-full"
+					>
+						<Page width={width ? width : 1} pageNumber={currPage} />
+					</Document>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default PdfRenderer;
